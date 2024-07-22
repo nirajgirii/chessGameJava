c04ababfab0e471dcb60a875dcdb8b6cf60a5b8c
Niraj Giri <nirajgiri@Nirajs-MacBook-Air.local>
2024-07-22 20:29:16 +0545

    Edit Logic for painting color in different lines (odd / even position logic)

--- main/Board.java
+++ main/Board.java
@@ -15,9 +15,11 @@ public class Board {
         for (int row = 0; row < MAX_ROW; row++) {
             for (int col = 0; col < MAX_COL; col++) {
                 if ((row + col) % 2 == 0) {
-                    g2.setColor(new Color(210, 165, 125)); // Light square color
+                    // Light square color
+                    g2.setColor(new Color(210, 165, 125));
                 } else {
-                    g2.setColor(new Color(175, 115, 70)); // Dark square color
+                    // Dark square color
+                    g2.setColor(new Color(175, 115, 70));
                 }
                 g2.fillRect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
             }
