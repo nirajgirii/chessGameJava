25e8e1fdadb0a36850653959bd598056b6c17ce8
Niraj Giri <nirajgiri@Nirajs-MacBook-Air.local>
2024-07-22 20:36:16 +0545

    Edit Jframe title

--- main/Main.java
+++ main/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
     public static void main(String[] args) {
-        JFrame window = new JFrame("Chess Game");
+        JFrame window = new JFrame("Chess Game by Niraj");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
 
