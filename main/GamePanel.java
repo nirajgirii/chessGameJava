3c5e61ced4a57e294ada1c936e90735bdc3f0ff6
Niraj Giri <nirajgiri@Nirajs-MacBook-Air.local>
2024-07-22 20:22:16 +0545

    Create board for the game

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,43 +1,47 @@
+package main;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
+
     public static final int WIDTH = 1100;
     public static final int HEIGHT = 800;
     final int FPS = 60;
     Thread gameThread;
+    Board board = new Board();
 
     public GamePanel() {
-       setPreferredSize(new Dimension(WIDTH, HEIGHT));
-       setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setBackground(Color.BLACK);
     }
 
     public void launchGame() {
         gameThread = new Thread(this);
         gameThread.start();
-    }   
+    }
 
     @Override
     public void run() {
 
-        //GAME LOOP
+        // GAME LOOP
 
         double drawInterval = 1000000000 / FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
 
-
-        while (gameThread != null){
+        while (gameThread != null) {
             currentTime = System.nanoTime();
 
             delta += (currentTime - lastTime) / drawInterval;
             lastTime = currentTime;
 
-            if (delta >= 1){
+            if (delta >= 1) {
                 update();
                 repaint();
                 delta--;
@@ -50,8 +54,11 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        // Call Board draw method
+        board.draw(g2);
     }
 
-
-   
 }
--- main/Main.java
+++ main/Main.java
@@ -1,20 +1,22 @@
+package main;
+
 import javax.swing.JFrame;
 
 public class Main {
 
     public static void main(String[] args) {
-        JFrame window = new JFrame("Chess Game");   
+        JFrame window = new JFrame("Chess Game");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
 
-//        Add Game Panel to the Window
-         GamePanel gp = new GamePanel();
-         window.add(gp);
-         window.pack();
+        // Add Game Panel to the Window
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-        
+
         gp.launchGame();
     }
 }
