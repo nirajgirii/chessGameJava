c110739d578112cd31c4d7531d8a99e03bdbd82b
Niraj Giri <nirajgiri@Nirajs-MacBook-Air.local>
2024-07-28 20:35:06 +0545

    Add Knight piece image in chess

---
+++ piece/Knight.java
@@ -0,0 +1,16 @@
+package piece;
+
+import main.GamePanel;
+
+public class Knight extends Piece {
+
+    public Knight(int color, int col, int row) {
+        super(color, col, row);
+
+        if(color == GamePanel.WHITE) {
+            image = getImage("res/piece/knight_white.png");
+        }else if(color == GamePanel.BLACK) {
+            image = getImage("res/piece/knight_black.png");
+        }
+    }
+}
