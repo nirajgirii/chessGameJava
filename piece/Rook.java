c11107599392ba0025a428db1854cc51bad329f4
Niraj Giri <nirajgiri@Nirajs-MacBook-Air.local>
2024-07-28 20:35:20 +0545

    Add Rook piece image in chess

---
+++ piece/Rook.java
@@ -0,0 +1,16 @@
+package piece;
+
+import main.GamePanel;
+
+public class Rook extends Piece{
+
+    public Rook(int color, int col, int row) {
+        super(color, col, row);
+
+        if(color == GamePanel.WHITE){
+            image = getImage("res/piece/rook_white.png");
+        }else if (color == GamePanel.BLACK){
+            image = getImage("res/piece/rook_black.png");
+        }
+    }
+}
