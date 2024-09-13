89a5d64cbd3dda967cb28924355c390022e39a4b
Niraj Giri <me.nirajgiri@gmail.com>
2024-09-13 21:23:09 +0545

    Create method canMove, and check if the move isWithinBoard or not then if the move hits another piece then act accordingly i.e if the piece is of same color then the move is invalid else if the piece is of another color then it can be captured easily.

--- piece/Piece.java
+++ piece/Piece.java
@@ -1,6 +1,7 @@
 package piece;
 
 import main.Board;
+import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -13,6 +14,7 @@ public class Piece {
     public int x, y;
     public int col, row, preCol, preRow;
     public int color;
+    public Piece hittingP;
 
     public Piece(int color, int col, int row) {
         this.color = color;
@@ -53,12 +55,64 @@ public class Piece {
         return (row + Board.HALF_SQUARE_SIZE) / Board.SQUARE_SIZE;
     }
 
-    public void updatePosition(){
+    public int getIndex() {
+        for (int i = 0; i < GamePanel.simPieces.size(); i++) {
+            if (GamePanel.simPieces.get(i) == this) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public void updatePosition() {
         x = getX(col);
         y = getY(row);
         preCol = getCol(x);
         preRow = getRow(y);
     }
+
+    public void resetPosition() {
+        col = preCol;
+        row = preRow;
+        x = getX(col);
+        y = getY(row);
+    }
+
+    public boolean canMove(int targetCol, int targetRow) {
+        return false;
+    }
+
+    public boolean isWithinBoard(int targetCol, int targetRow) {
+        if (targetCol >= 0 && targetCol <= 7 && targetRow >= 0 && targetRow <= 7) {
+            return true;
+        }
+        return false;
+    }
+
+    public Piece getHittingP(int targetCol, int targetRow) {
+        for (Piece piece : GamePanel.simPieces) {
+            if (piece.col == targetCol && piece.row == targetRow && piece != this) {
+                return piece;
+            }
+        }
+        return null;
+    }
+
+    public boolean isValidSquare(int targetCol, int targetRow) {
+        hittingP = getHittingP(targetCol, targetRow);
+
+        if (hittingP == null) {
+            return true;
+        } else {
+            if (hittingP.color != this.color) {
+                return true;
+            } else {
+                hittingP = null;
+            }
+        }
+        return false;
+    }
+
 
     public void draw(Graphics2D g2) {
         g2.drawImage(image, x, y, Board.SQUARE_SIZE, Board.SQUARE_SIZE, null);
