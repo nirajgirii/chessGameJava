e25544a83c896573ad2e6225c042a72a80dfd2f9
Niraj Giri <me.nirajgiri@gmail.com>
2024-09-14 19:04:44 +0545

    Create canMove method for Knight to move in L shape

--- piece/Knight.java
+++ piece/Knight.java
@@ -13,4 +13,15 @@ public class Knight extends Piece {
             image = getImage("/res/piece/knight_black.png");
         }
     }
+    public boolean canMove(int targetCol, int targetRow) {
+        if (isWithinBoard(targetCol, targetRow)) {
+            // Knight can move if its movement ratio of col and row is 2:1 or 1:2
+            if (Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2) {
+                if (isValidSquare(targetCol, targetRow)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
