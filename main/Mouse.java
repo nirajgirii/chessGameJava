2ffbbc5784c185fb03d1c373b89d49d79293abc2
Niraj Giri <me.nirajgiri@gmail.com>
2024-09-26 12:30:40 +0545

    add hasClick to handle click methods

--- main/Mouse.java
+++ main/Mouse.java
@@ -1,31 +1,31 @@
 package main;
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Point;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Mouse extends MouseAdapter {
     public int x, y;
-    public boolean pressed;
+    public ConcurrentLinkedQueue<Point> clickQueue = new ConcurrentLinkedQueue<>();
 
     @Override
     public void mousePressed(MouseEvent e) {
-        pressed = true;
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        pressed = false;
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
         x = e.getX();
         y = e.getY();
+        clickQueue.offer(new Point(x, y));
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
         x = e.getX();
         y = e.getY();
+    }
+
+    public boolean hasClick() {
+        return !clickQueue.isEmpty();
+    }
+
+    public Point getNextClick() {
+        return clickQueue.poll();
     }
 }
